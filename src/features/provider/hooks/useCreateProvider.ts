5122808401eca88ba6e9c6eca80647e63c438f83
geymomati <geymomati@gmail.com>
2025-11-08 18:23:26 -0300

    feat(provider/hooks): añade y ajusta hooks de proveedores

--- src/features/provider/hooks/useCreateProvider.ts
+++ src/features/provider/hooks/useCreateProvider.ts
@@ -1,24 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { createProvider } from "@/features/provider/api/service";
 import { CreateProviderRequest, CreateProviderResponse } from "@/features/provider/types";
-import { useToast } from "@/shared/hooks/useToast";
 
 export const useCreateProvider = () => {
 
     const queryClient = useQueryClient();
-    const { showToast } = useToast();
 
     return useMutation<CreateProviderResponse, Error, CreateProviderRequest>({
         mutationFn: createProvider,
-        onSuccess: (data) => {
+        onSuccess: () => {
             queryClient.invalidateQueries({ queryKey: ['providers'] });
-            showToast(
-                `Proveedor ${data.data.firstName} creado exitosamente`,
-                'success'
-            );
-        },
-        onError: (error) => {
-            showToast(`Error al crear proveedor: ${error.message}`, 'error');
         },
     });
 
