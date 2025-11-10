c6b6679c1bbf6bef5c1b9448a17970ddc8867f6b
geymomati <geymomati@gmail.com>
2025-11-10 13:10:23 -0300

    feat(provider/hooks): añade hook para búsqueda de proveedor por CUIT

--- src/features/provider/hooks/useProviderByCuit.ts
+++ src/features/provider/hooks/useProviderByCuit.ts
@@ -1,13 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import { getProviderByCuit } from '@features/provider/api/service';
-import { GetProviderResponse } from '@features/provider/types';
+import { GetProviderResponse, Provider } from '@features/provider/types';
 
 export const useProviderByCuit = (cuit: number | undefined) => {
-  return useQuery<GetProviderResponse>({
+  const { data, isLoading, error } = useQuery<GetProviderResponse>({
     queryKey: ['provider', cuit],
     queryFn: () => getProviderByCuit(cuit!),
     enabled: !!cuit,
     staleTime: 5 * 60 * 1000,
     refetchOnWindowFocus: false,
   });
+
+  return {
+    provider: data?.data as Provider | undefined,
+    isLoading,
+    error,
+  };
 };
